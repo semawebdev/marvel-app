0b7a6fc415e71602b1ec32a20aa26fab9dbd620e
semawebdev <isemagul@gmail.com>
2023-06-13 19:01:46 +0200

    improve UI

--- src/components/HeroesTable/HeroCard.js
+++ src/components/HeroesTable/HeroCard.js
@@ -3,7 +3,7 @@ import { Feature } from "./Feature"
 export const HeroCard = ({ hero, feature }) => {
     const thumbnailSrcPath = `${hero.thumbnail.path}/portrait_xlarge.jpg`;
     return (
-        <div>
+        <div className="heroCard">
             <img alt={hero.name} src={thumbnailSrcPath} />
             <Feature hero={hero} feature={feature}/>
         </div>
