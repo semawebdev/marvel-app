770c2a252a0833dcc6342a66bae0d0f1d4a5cf77
semawebdev <isemagul@gmail.com>
2023-06-13 17:14:58 +0200

    improve UI

--- src/components/FeatureSelect/FeatureSelect.js
+++ src/components/FeatureSelect/FeatureSelect.js
@@ -3,15 +3,16 @@ import { features } from '../../constants';
 export const FeatureSelect = ({ feature, onChange }) => {
     return (
       <>
-        <label htmlFor="feature-select">Choose a feature:</label>
+        <label for="feature-select" className="label">Choose a feature:</label>
   
         <select
           name="feature"
           id="feature-select"
           onChange={onChange}
           value={feature}
+          className="dropdown"
         >
-          <option value="">--Please choose an option--</option>
+          <option value="">Please choose an option</option>
           {Object.entries(features).map(([key, value]) => (
             <option key={key} value={value}>
               {value}
